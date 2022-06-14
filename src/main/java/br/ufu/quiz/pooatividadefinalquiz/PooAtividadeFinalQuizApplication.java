ab02ce83736ff1eecae7c15f60538e6f52b46867
Aureom <kaioaugusto02@gmail.com>
2022-06-14 19:02:11 -0300

    🎉 Create Spring Project

---
+++ src/main/java/br/ufu/quiz/pooatividadefinalquiz/PooAtividadeFinalQuizApplication.java
@@ -0,0 +1,13 @@
+package br.ufu.quiz.pooatividadefinalquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooAtividadeFinalQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooAtividadeFinalQuizApplication.class, args);
+	}
+
+}
