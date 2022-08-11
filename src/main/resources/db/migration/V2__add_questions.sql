efe86fcf4fe379eab52b8fbf5b9c0bcd015fc303
Kaio Souza <kaio@biomainvestimentos.com>
2022-08-10 22:29:46 -0300

    CHORE: add new questions

---
+++ src/main/resources/db/migration/V2__add_questions.sql
@@ -0,0 +1,7 @@
+INSERT INTO question(id, question, optiona, optionb, optionc, optiond, correct_answer) VALUES
+('4a78735a-b37b-40f0-9218-a9481bdb7ebe', 'Qual é a capital da frança?', 'Londres', 'Paris', 'Paraná', 'Baguette', '1'),
+('9fac0e29-a226-4c30-b114-4c029c01c2cb', 'O que é a Via Láctea?', 'Uma galáxia', 'Uma marca de leite', 'Um tipo de carne', 'Uma rodovia', '0'),
+('9f370d76-7422-4029-9d4b-dd66e7ac894a', 'Em que dia nasceu Bill Gates?', '28 de setembro de 1957', '28 de outubro de 1955', '28 de fevereiro de 1960', '30 de outubro de 1950', '1'),
+('068a748b-92dd-4be0-836e-87ffb4370f80', 'Em que ano foi fundada a Riot Games?', '2003', '2009', '2006', '2005', '2'),
+('03300f32-9380-4ed8-bbdf-9c4394a4f4c2', 'O satélite artificial Sputnik foi lançado em que ano?', '1957', '1958', '1959', '1960', '0'),
+('114d78d9-1f7c-496f-8b6e-e97e7bca1e13', 'Qual o nome do telescopio lançado em 25 de Dezembro de 2021?', 'James Webb', 'Hubble', 'New Horizons', 'Kepler', '0');
